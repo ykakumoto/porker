cb03eacfec58924753af5fa6e5a5e5a87144ddfc
角元 友樹 <ykakumoto@netprotections.co.jp>
2016-10-21 19:49:53 +0900

    簡易的な入力画面・出力画面と、処理ロジックの大枠の構築

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -4,55 +4,54 @@ class CheckController < ApplicationController
 
   def make_array_chara(input)
     # 入力された配列から、文字のみの配列を作成
-    @chara = []
-    input.each do |input|
-      @chara << /\D*/.match(input)
+    chara = []
+    input.each do |i|
+      chara << i[0]
     end
 
-    return @chara
+    return chara
 
   end
 
 
   def make_array_num(input)
     # 入力された配列から、数字のみの配列を作成
-    @num = []
-    input.each do |input|
-      @num << /\d+/.match(input)
+    num = []
+    input.each do |i|
+      i.slice!(0)
+      num << i
     end
 
-    return @num
+    return num
 
   end
 
 
   def pairs_count(input)
     # 入力された配列のペア数をカウント
-    @i   = 0
-    @u   = 0
-    @sum = 0
-    for @i in 0..3 do
-      for @u in @i+1..4 do
-        @sum += 1 if (input[@i] == input[@u])
+    i   = 0
+    u   = 0
+    sum = 0
+    for i in 0..3 do
+      for u in i+1..4 do
+        sum += 1 if (input[i] == input[u])
       end
-      return @sum
     end
+
+    return sum
+
   end
 
-  # def straight?(numbers)
-  #   # 入力された配列がストレートになっているかを確認
-  #   if (numbers.max - numbers.min == 4 && pairs_count(numbers) == 0)
-  #     # ||
-  #     # (numbers.sort == [1, 10, 11, 12, 13])
-  #   then
-  #     return true
-  #     　
-  #   else
-  #     return false
-  #
-  #   end
-  #
-  # end
+  
+  def straight?(numbers)
+    # 入力された配列がストレートになっているかを確認
+    if (numbers.max.to_i - numbers.min.to_i == 4 && pairs_count(numbers) == 0) || (numbers.sort.map(&:to_i) == [1, 10, 11, 12, 13])
+      return true
+    else
+      return false
+    end
+
+  end
 
 
   def handcheck
@@ -64,34 +63,26 @@ class CheckController < ApplicationController
     @numbers_pairs = pairs_count(@numbers)
 
 
-#   if (@suits_pairs == 10 && straight?(@numbers))
-#   then
-#     @hand = "ストレートフラッシュ"
-#   elsif (@numbers_pairs == 6)
-#   then
-#     @hand = "フォー・オブ・ア・カインド"
-#   elsif (@numbers_pairs == 4)
-#   then
-#     @hand = "フルハウス"
-#   elsif (@suits_pairs == 10)
-#   then
-#     @hand == "フラッシュ"
-#   elsif (straight?(@numbers))
-#   then
-#     @hand = "ストレート"
-#   elsif (@numbers_pairs == 3)
-#   then
-#     @hand = "スリー・オブ・ア・カインド"
-#   elsif (@numbers_pairs == 2)
-#   then
-#     @hand = "ツーペア"
-#   elsif (@numbers_pairs == 1)
-#   then
-#     @hand = "ワンペア"
-#   else
-#     @hand = "ハイガード"
-#   end
-#
+    if (@suits_pairs == 10 && straight?(@numbers))
+      @hand = "ストレートフラッシュ"
+    elsif (@numbers_pairs == 6)
+      @hand = "フォー・オブ・ア・カインド"
+    elsif (@numbers_pairs == 4)
+      @hand = "フルハウス"
+    elsif (@suits_pairs == 10)
+      @hand == "フラッシュ"
+    elsif (straight?(@numbers))
+      @hand = "ストレート"
+    elsif (@numbers_pairs == 3)
+      @hand = "スリー・オブ・ア・カインド"
+    elsif (@numbers_pairs == 2)
+      @hand = "ツーペア"
+    elsif (@numbers_pairs == 1)
+      @hand = "ワンペア"
+    else
+      @hand = "ハイカード"
+    end
+
   end
 
 
